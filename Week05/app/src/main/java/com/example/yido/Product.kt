4da7ebc285d6e03372896a2131370faaa59a5ca7
YiDo <umckee5696@gmail.com>
2026-04-28 15:50:01 +0900

    Fix: 기존 위치의 중복 파일 삭제 반영

--- Week05/app/src/main/java/com/example/yido/Product.kt
+++
@@ -1,9 +0,0 @@
-package com.example.yido
-
-data class Product(
-    val imageResId: Int,
-    val name: String,
-    val price: String,
-    val isWished: Boolean = false,
-    val category: String = "?„ì²´"   // "?„ì²´" | "Tops & T-Shirts" | "sale"
-)
