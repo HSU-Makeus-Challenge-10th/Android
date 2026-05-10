468e265ec89b942672e28f6eefd91e59a34da341
choiwonjae <chldnjswo1234@naver.com>
2026-05-10 18:39:39 +0900

    [feat]데쿠 7주차

---
+++ Week07/Deku/app/src/main/java/com/example/deku/core/common/AppConstants.kt
@@ -0,0 +1,6 @@
+package com.example.deku.core.common
+
+const val EXTRA_HOME_TITLE = "com.example.deku.extra.HOME_TITLE"
+const val SPLASH_HOME_TITLE = "NIKE"
+
+internal const val DEFAULT_HOME_TITLE = "Discover"
